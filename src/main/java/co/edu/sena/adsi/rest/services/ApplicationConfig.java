c51741eb44a6a9f5cf90590effc5fcb621bc80c5
DavidSJamauca <dsjamauca@misena.edu.co>
2017-08-26 16:40:45 -0500

    Backend

---
+++ src/main/java/co/edu/sena/adsi/rest/services/ApplicationConfig.java
@@ -0,0 +1,18 @@
+
+package co.edu.sena.adsi.rest.services;
+
+import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
+
+/**
+ *
+ * @author David Jamauca
+ */
+@ApplicationPath("api")
+public class ApplicationConfig extends ResourceConfig{
+    
+    public ApplicationConfig(){
+        
+    }
+    
+}
---
+++ target/generated-sources/annotations/co/edu/sena/adsi/jpa/entities/TipoDocumento_.java
@@ -0,0 +1,14 @@
+package co.edu.sena.adsi.jpa.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-08-26T16:32:28")
+@StaticMetamodel(TipoDocumento.class)
+public class TipoDocumento_ { 
+
+    public static volatile SingularAttribute<TipoDocumento, String> descripcion;
+    public static volatile SingularAttribute<TipoDocumento, Integer> id;
+
+}
